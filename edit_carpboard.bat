cdacb43520cea1804a9d7693855bce0374a7456f
wyattscarpenter <wyattscarpenter@gmail.com>
2024-09-17 15:05:44 -0700

    Add a little supporting utility script

---
+++ edit_carpboard.bat
@@ -0,0 +1,2 @@
+REM Listen... this script uses notepad++ to open the shortcut that probably points to the carpboard file. It's hyperoptimized for my specific use because I got tired of closing and opening (and keeping open) the file in my editor in inefficient way.
+"C:\Program Files (x86)\Notepad++\notepad++.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\carpboard - Shortcut.lnk"
